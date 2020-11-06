8940c311d97754093cccbcd79e1e63d0da186f88
TerryOPP <61347225+terryopp@users.noreply.github.com>
2020-11-07 01:55:13 +0400

    Реализация создания таблицы

--- js/ControlTable.js
+++ js/ControlTable.js
@@ -1,3 +1,27 @@
 function ControlTable () { // Конструктор класса для вывода таблицы попарного сравнения
+  const table = document.getElementById('answer');
 
+  this.creatingTable = (crit) => { // Создание таблицы
+    let amount = crit.length;
+  }
+
+  this.outputTable = (crit) => {
+    let amount = crit.length; 
+    for (let i = 0; i < amount; i++) {
+      let row = table.insertRow(-1);
+      for (let j = 0; j < amount; j++) {
+        let cell = row.insertCell(-1);
+        if (i == 0) cell.append(crit[j]);
+        if (i > 0 && j == 0) cell.append(crit[i]);
+        if (i > 0 && i == j) cell.append('1');
+      }
+    }
+  }
+
+  this.deletingTable = () => {
+    while(table.rows.length > 0) {
+      table.deleteRow(0);
+    }
+    console.log(table.rows.length);
+  }
 }
--- js/main.js
+++ js/main.js
@@ -1,12 +1,16 @@
 window.onload = function() {
   const text = document.getElementById('text'); // Поле ввода критериев
-  const crit = []; // Массив критериев
+  const crit = ['']; // Массив критериев
+
+  const table = new ControlTable();
 
   text.addEventListener('keydown', (e)=>{ // Определение нажатия на Enter и добавление критерия в список
     if (e.code == "Enter" && text.value != "") {
+      table.deletingTable();
       crit.push(text.value);
       text.value = '';
       console.log(crit);
+      table.outputTable(crit);
     }
   });
 }
