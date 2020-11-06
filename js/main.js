6e3cf7f4cd23d1b27f7af870e9c30613572bde9b
TerryOPP <61347225+terryopp@users.noreply.github.com>
2020-11-07 00:26:24 +0400

    Создание простейшего алгоритма выявления введенного текстика

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
 window.onload = function() {
-    let input = document.getElementById('text'); // Ввод текста
-    let output = document.getElementById('output'); // Вывод текста
+    const text = document.getElementById('text'); // Поле ввода критериев
+    const crit = []; // Массив критериев
+
+    text.addEventListener('keydown', (e)=>{ // Определение нажатия на Enter и добавление критерия в список
+        if (e.code == "Enter" && text.value != "") {
+            crit.push(text.value);
+            text.value = '';
+            console.log(crit);
+        }
+    });
 }
