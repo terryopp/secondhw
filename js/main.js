6f799c7a05d54fd6c565d0d1ecdba8ef4e0c16ae
TerryOPP <61347225+terryopp@users.noreply.github.com>
2020-11-07 00:34:54 +0400

    Создание памятки критериев оценивания

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 window.onload = function() {
-    const text = document.getElementById('text'); // Поле ввода критериев
-    const crit = []; // Массив критериев
+  const text = document.getElementById('text'); // Поле ввода критериев
+  const crit = []; // Массив критериев
 
-    text.addEventListener('keydown', (e)=>{ // Определение нажатия на Enter и добавление критерия в список
-        if (e.code == "Enter" && text.value != "") {
-            crit.push(text.value);
-            text.value = '';
-            console.log(crit);
-        }
-    });
+  text.addEventListener('keydown', (e)=>{ // Определение нажатия на Enter и добавление критерия в список
+    if (e.code == "Enter" && text.value != "") {
+      crit.push(text.value);
+      text.value = '';
+      console.log(crit);
+    }
+  });
 }
